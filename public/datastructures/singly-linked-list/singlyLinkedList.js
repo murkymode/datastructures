7737e1e4accfa9569ccf3884031745ead6f70907
Jason <jasonmarble@gmail.com>
2021-10-20 16:12:33 -0600

    relocates datastructures folder, adds linked list framework

---
+++ public/datastructures/singly-linked-list/singlyLinkedList.js
@@ -0,0 +1,37 @@
+/*
+singly linked list
+es6 pattern
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = this.head;
+    this.length = 0;
+  }
+
+  push(value) {
+  }
+
+  pop(value) {
+  }
+
+  insert(value) {
+  }
+
+  lookup(index) {
+  }
+
+  remove(index) {
+  }
+
+  reverse() {
+  }
+}
