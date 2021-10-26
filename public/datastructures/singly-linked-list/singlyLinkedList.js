8b4d2fd5c817e99996978917a3901a904582ffdc
Jason <jasonmarble@gmail.com>
2021-10-26 16:58:10 -0600

    finished SLL starts DLL

--- public/datastructures/singly-linked-list/singlyLinkedList.js
+++ public/datastructures/singly-linked-list/singlyLinkedList.js
@@ -1,8 +1,3 @@
-/*
-singly linked list
-es6 pattern
-*/
-
 import Node from './node';
 
 class SinglyLinkedList {
@@ -14,6 +9,7 @@ class SinglyLinkedList {
 
   push(value) { /* new tail */
     const newNode = new Node(value);
+
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
@@ -21,35 +17,44 @@ class SinglyLinkedList {
       this.tail.next = newNode;
       this.tail = newNode;
     }
+
     this.length += 1;
   }
 
   pop() { /* remove tail */
     if (!this.head) return undefined;
+
     let current = this.head;
     let newTail = current;
+
     while (current.next) {
       newTail = current;
       current = current.next;
     }
+
     this.tail = newTail;
     this.tail.next = null;
     this.length -= 1;
+
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
+
     return current;
   }
 
   shift() { /* remove head */
     if (!this.head) return undefined;
+
     const currentHead = this.head;
     this.head = currentHead.next;
     this.length -= 1;
+
     if (this.length === 0) {
       this.tail = null;
     }
+
     return currentHead;
   }
 
@@ -64,32 +69,39 @@ class SinglyLinkedList {
     if (index < 0 || index > this.length) return false;
     if (index === this.length) return !!this.push(value);
     if (index === 0) return !!this.unshift(value);
+
     const newNode = new Node(value);
     const priorNode = this.get(index - 1);
     const temp = priorNode.next;
     priorNode.next = newNode;
     newNode.next = temp;
     this.length += 1;
+
     return true;
   }
 
   get(index) { /* value at */
     if (index < 0 || index >= this.length) return null;
+
     let current = this.head;
     let counter = 0;
+
     while (counter !== index) {
       current = current.next;
       counter += 1;
     }
+
     return current;
   }
 
   set(index, value) { /* alter value at */
     const targetNode = this.get(index);
+
     if (targetNode) {
       targetNode.value = value;
       return true;
     }
+
     return false;
   }
 
@@ -97,10 +109,12 @@ class SinglyLinkedList {
     if (index < 0 || index >= this.length) return undefined;
     if (index === 0) return this.shift();
     if (index === this.length - 1) return this.pop();
+
     const priorNode = this.get(index - 1);
     const removed = priorNode.next;
     priorNode.next = removed.next;
     this.length -= 1;
+
     return removed;
   }
 
@@ -110,12 +124,14 @@ class SinglyLinkedList {
     this.tail = node;
     let next;
     let prev = null;
+
     for (let i = 0; i < this.length; i += 1) {
       next = node.next;
       node.next = prev;
       prev = node;
       node = next;
     }
+
     return this;
   }
 }
