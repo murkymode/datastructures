26638b3fa2726e544c3565a6c7fea6c49fc01cc0
Jason <jasonmarble@gmail.com>
2021-12-03 16:02:06 -0700

    reconfigs to make more sense

--- public/datastructures/__tests__/singlyLinkedList.test.js
+++ public/datastructures/__tests__/singlyLinkedList.test.js
@@ -24,16 +24,16 @@ describe('Singly Linked List', () => {
 
   test('has working remove head method', () => {
     expect(typeof testList.shift).toBe('function');
-    testList.push(2);
-    testList.push(3);
+    testList.push(4);
+    testList.push(5);
     testList.shift();
     expect(testList.head.value).toBe(3);
   });
 
   test('has working create new head method', () => {
     expect(typeof testList.unshift).toBe('function');
-    testList.unshift(2);
-    expect(testList.head.value).toBe(2);
+    testList.unshift(1);
+    expect(testList.head.value).toBe(1);
   });
 
   test('has working retrieve value at target method', () => {
@@ -50,6 +50,7 @@ describe('Singly Linked List', () => {
 
   test('has remove node method', () => {
     expect(typeof testList.remove).toBe('function');
+
   });
 
   test('has reverse list method', () => {
