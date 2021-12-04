9786a134627e4e06f5e9fa567fff32a3b497aa0f
Jason <jasonmarble@gmail.com>
2021-12-04 15:42:56 -0700

    sets up test connection

---
+++ datastructures/__tests__/DLL/doublyLinkedList.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const { DoublyLinkedList } = require('../../doubly-linked-list');
+
+describe('Doubly Linked List', () => {
+  test('should be a function', () => {
+    expect(typeof DoublyLinkedList).toBe('function');
+  });
+});
