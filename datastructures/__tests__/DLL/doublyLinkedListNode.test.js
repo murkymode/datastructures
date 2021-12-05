c77cfbfcc3dbe7959e5466f91a1c46e378bac29f
Jason <jasonmarble@gmail.com>
2021-12-04 18:37:08 -0700

    expands check on Node class

--- datastructures/__tests__/DLL/doublyLinkedListNode.test.js
+++ datastructures/__tests__/DLL/doublyLinkedListNode.test.js
@@ -2,7 +2,10 @@
 const { Node } = require('../../doubly-linked-list/node');
 
 describe('Doubly Linked List Node', () => {
-  test('should be a function', () => {
+  test('should be a constructor function', () => {
     expect(typeof Node).toBe('function');
+    const testNode = new Node();
+    const isInstance = testNode instanceof Node;
+    expect(isInstance).toBe(true);
   });
 });
