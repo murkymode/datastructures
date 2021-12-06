9e3cb199e7191b295e742b5198566fc1bb661931
Jason <jasonmarble@gmail.com>
2021-12-06 12:12:02 -0700

    trims comments

--- datastructures/doubly-linked-list/index.js
+++ datastructures/doubly-linked-list/index.js
@@ -8,15 +8,8 @@ class DoublyLinkedList {
   }
 
   push(value) {
-    // create a new node with the given value
     const newNode = new Node(value);
-    // if list has no head
-      // current head is new node
-      // current tail is new node
-    // otherwise
-      // current tail's next is new node
-      // new node's prev is current tail
-      // current tail is new node
+
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
@@ -25,8 +18,7 @@ class DoublyLinkedList {
       newNode.prev = this.tail;
       this.tail = newNode;
     }
-    // increment length
-    // return list
+
     this.length += 1;
     return this;
   }
