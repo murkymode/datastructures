85fc7ca4856c382b993d89a1d613a12d50212faf
Jason <jasonmarble@gmail.com>
2021-12-06 12:11:43 -0700

    passed tests for push

--- datastructures/doubly-linked-list/index.js
+++ datastructures/doubly-linked-list/index.js
@@ -9,6 +9,7 @@ class DoublyLinkedList {
 
   push(value) {
     // create a new node with the given value
+    const newNode = new Node(value);
     // if list has no head
       // current head is new node
       // current tail is new node
@@ -16,8 +17,18 @@ class DoublyLinkedList {
       // current tail's next is new node
       // new node's prev is current tail
       // current tail is new node
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
     // increment length
     // return list
+    this.length += 1;
+    return this;
   }
 }
 
