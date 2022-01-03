97ece6fe9dcaba03e40a900ddc511c33eb374636
Jason <jasonmarble@gmail.com>
2022-01-03 15:35:05 -0700

    adds node tests

--- src/__tests__/tree/treeNode.test.js
+++ src/__tests__/tree/treeNode.test.js
@@ -4,4 +4,13 @@ const { Node } = require('../../tree/node');
 const testNode = new Node();
 
 describe('Tree Node', () => {
+  test('should be a constructor function', () => {
+    expect(typeof Node).toBe('function');
+    const isInstance = testNode instanceof Node;
+    expect(isInstance).toBe(true);
+  });
+
+  test('should initialize all values at null', () => {
+    expect(testNode.root).toBe(null);
+  });
 });
