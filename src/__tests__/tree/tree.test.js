c91e010a504edc4bf28cdb5c797f3a68e547f34b
Jason <jasonmarble@gmail.com>
2022-01-03 20:41:25 -0700

    adds first set of tests

--- src/__tests__/tree/tree.test.js
+++ src/__tests__/tree/tree.test.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-undef */
 const { Tree } = require('../../tree');
+const { Node } = require('../../tree/node');
 
-const testTree = new Tree();
+const testTree = new Tree(5);
 
 describe('Doubly Linked List', () => {
   test('should be a constructor function', () => {
     expect(typeof Tree).toBe('function');
     const isInstance = testTree instanceof Tree;
     expect(isInstance).toBe(true);
+  });
+
+  test('should initialize root correctly', () => {
+    expect(testTree.root).toBe(5);
+    expect(testTree.children.length).toBe(0);
+  });
+
+  test('should be able to add child node', () => {
+    const testNode = new Node(7);
+    testTree.addChild(testNode);
+    expect(testTree.children[0].value).toBe(7);
+  });
+
+  xtest('', () => {
   });
 });
