406c9add7058368c58cd6d3456aa1f35cf8c2575
murkymode <jasonmarble@gmail.com>
2024-01-19 19:28:48 -0500

    adds enqueue, dequeue, and size methods

--- src/stacks_queues/queue.js
+++ src/stacks_queues/queue.js
@@ -4,4 +4,21 @@ class Queue {
     this.frontOfQueue = 0;
     this.backOfQueue = 0;
   }
+
+  enqueue(value) {
+    this.storage[this.backOfQueue] = value;
+    this.backOfQueue++;
+  }
+
+  dequeue() {
+    const deleted = this.storage[this.frontOfQueue];
+    delete this.storage[this.frontOfQueue];
+    this.frontOfQueue++;
+    return deleted;
+  }
+
+  size() {
+    const size = this.backOfQueue - this.frontOfQueue;
+    return size > 0 ? size : 0;
+  }
 }
