6eea01a94acd79e81a8ea80a76d558e52f53ab77
murkymode <jasonmarble@gmail.com>
2024-01-19 19:11:47 -0500

    adds pop and size; formatting

--- src/stacks_queues/stack.js
+++ src/stacks_queues/stack.js
@@ -6,8 +6,18 @@ class Stack {
   }
 
   push(value) {
-
     this.storage[this.index] = value;
     this.index++;
+  }
+
+  pop() {
+    this.index--;
+    var deleted = this.storage[this.index];
+    delete this.storage[this.index];
+    return deleted;
+  }
+
+  size() {
+    return this.index > 0 ? this.index : 0;
   }
 }
